cf376ac9d98f179dca3c2c7a950756a0604657b2
alramalho <alexandre.ramalho.1998@gmail.com>
2021-01-30 18:27:42 +0000

    Stable

--- src/components/chart.js
+++ src/components/chart.js
@@ -21,53 +21,64 @@ const AMOUNT_STEP = 1000
 const MONTHLY_PROFIT_STEP = 0.5
 const MONTHLY_INJECTION_STEP = 50
 
-
-function generateProfit(durationInYears, initialAmmount, monthlyProfit, montlyInjection) {
-  return [...Array(durationInYears * 12).keys()].reduce((accumulator) => {
-    return (accumulator + montlyInjection) * (1 + (monthlyProfit / 100))
-  }, initialAmmount)
+function generateProfit(durationInYears, initialAmount, monthlyProfit, monthlyInjection) {
+  const result = Array(((durationInYears) || 0)*12).fill(1).reduce((accumulator) => {
+    return (accumulator + monthlyInjection) * (1 + (monthlyProfit / 100))
+  }, initialAmount)
+  console.log(`total: for duration ${durationInYears}, amount ${initialAmount}, profit ${monthlyProfit}, injection ${monthlyInjection} `, result)
+  return result
 }
 
 const Chart = () => {
-  const [duration, setDuration] = useState(5)
-  const [initialAmount, setInitialAmount] = useState(10000)
-  const [profit, setProfit] = useState(0.5)
-  const [injection, setInjection] = useState(12345)
-  const [xAxis, setXAxis] = useState(SELECT_OPTIONS.DURATION)
-  const [xAxisData, setXAxisData] = useState([
-    {name: 'Page A', pv: 2400},
-    {name: 'Page B', pv: 1398},
-    {name: 'Page C', pv: 9800},
-    {name: 'Page D', pv: 3908},
-    {name: 'Page E', pv: 4800},
-    {name: 'Page F', pv: 3800},
-    {name: 'Page G', pv: 4300},
-  ])
+  const [duration, setDuration] = useState(3)
+  const [initialAmount, setInitialAmount] = useState(10001)
+  const [profit, setProfit] = useState(2)
+  const [injection, setInjection] = useState(400)
+  const [xAxisOption, setXAxisOption] = useState(SELECT_OPTIONS.AMOUNT)
+  const [xAxisData, setXAxisData] = useState(null)
 
-  useEffect(() => {
+  function generateNewData(option) {
     const newData = []
-    switch (xAxis) {
+    let step, value
+    switch (option){
       case SELECT_OPTIONS.DURATION:
-        for (let i = 0; i <= 6; i++) {
-          const value = parseInt(duration) - 3*DURATION_STEP + i*DURATION_STEP
-          newData.push({
-            name: value + ' years',
-            total: generateProfit(value, 10000, 2, 100)
-          })
-        }
-        setXAxisData(newData)
+        step = DURATION_STEP;
+        value = duration;
         break;
       case SELECT_OPTIONS.AMOUNT:
-        for (let i = 0; i <= 6; i++) {
-          const value = parseInt(initialAmount) - 3*AMOUNT_STEP + i*AMOUNT_STEP
-          newData.push({
-            name: value + '€',
-            total: generateProfit(duration, initialAmount, 2, 100) + " €"
-          })
-        }
-        setXAxisData(newData)
+        step = AMOUNT_STEP;
+        value = initialAmount;
+        break;
+      case SELECT_OPTIONS.PROFIT:
+        step = MONTHLY_PROFIT_STEP;
+        value = profit;
+        break
+      case SELECT_OPTIONS.INJECTION:
+        step = MONTHLY_INJECTION_STEP;
+        value = injection;
+        break;
     }
-  }, [duration, initialAmount, profit, injection])
+    for (let i = 0; i <= 6; i++) {
+      const steppedValue = Number(value) - 3 * step + i * step
+      // console.log(`step: ${step}, value: ${value}, steppedValue: ${steppedValue}`)
+      newData.push({
+        name: `${steppedValue}${option === SELECT_OPTIONS.DURATION ? ' years' : option === SELECT_OPTIONS.PROFIT ? '%' : '€'}`,
+        total: generateProfit(
+          option === SELECT_OPTIONS.DURATION ? steppedValue : duration,
+          option === SELECT_OPTIONS.AMOUNT ? steppedValue : initialAmount,
+          option === SELECT_OPTIONS.PROFIT? steppedValue : profit,
+          option === SELECT_OPTIONS.INJECTION ? steppedValue : injection,
+        )
+      })
+    }
+    console.log("Data generated: ", newData)
+    return newData
+  }
+
+  useEffect(() => {
+    setXAxisData(generateNewData(xAxisOption))
+    console.log(`the enum selected was ${xAxisOption}`)
+  }, [duration, initialAmount, profit, injection, xAxisOption])
 
 
   return (
@@ -84,8 +95,8 @@ const Chart = () => {
           <CartesianGrid strokeDasharray="3 3"/>
           <Area type="monotone" dataKey="total" stroke="#f1a208" fill="#f1a208"/>
           <XAxis dataKey="name"/>
-          <YAxis unit={"€"}/>
-          <Tooltip formatter={(value) => parseInt(value).toString() + '€'}/>
+          <YAxis form unit={'€'} width={70}/>
+          <Tooltip formatter={(value) => `${parseInt(value)}€`}/>
         </AreaChart>
       </ResponsiveContainer>
       <HorizontalContainer>
@@ -96,9 +107,9 @@ const Chart = () => {
               native
               labelId="demo-simple-x-select"
               id="demo-simple-x-select"
-              value={xAxis}
+              value={xAxisOption}
               onChange={event => {
-                setXAxis(event.target.value)
+                setXAxisOption(event.target.value)
               }}
             >
               <option value={SELECT_OPTIONS.DURATION}>Duration of investment</option>
@@ -111,28 +122,28 @@ const Chart = () => {
         <VerticalContainer>
           <NumInput
             value={duration}
-            onChange={(value) => setDuration(value)}
+            onChange={(value) => {setDuration(value > 100 ? 100 : value < 0 ? 0 : value)}}
             label={"Duration of investment"}
             endAdornment={<InputAdornment position="end">years</InputAdornment>}
             labelWidth={200}
           />
           <NumInput
             value={initialAmount}
-            onChange={(value) => setInitialAmount(value)}
+            onChange={(value) => setInitialAmount(parseInt(value))}
             label="Initial amount"
             startAdornment={<InputAdornment position="start">€</InputAdornment>}
             labelWidth={120}
           />
           <NumInput
             value={profit}
-            onChange={(value) => setProfit(value)}
+            onChange={(value) => setProfit(parseFloat(value))}
             label="Monthly profit"
-            startAdornment={<InputAdornment position="start">€</InputAdornment>}
+            startAdornment={<InputAdornment position="start">%</InputAdornment>}
             labelWidth={130}
           />
           <NumInput
             value={injection}
-            onChange={(value) => setInjection(value)}
+            onChange={(value) => setInjection(parseInt(value))}
             label="Monthly injection"
             startAdornment={<InputAdornment position="start">€</InputAdornment>}
             labelWidth={150}
